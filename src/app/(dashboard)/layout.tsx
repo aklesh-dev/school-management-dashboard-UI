f4bf773beeb70bad1b554827879cf24f51e88da8
aklesh-dev <arzunayavadu998@gmail.com>
2024-12-16 12:18:56 +0545

    Tailwind Responsive Layout - create a component navbar. - divide the left and right side width and make responsive for each device.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (    
-      <div>{children}</div>    
+      <div className="h-screen flex">
+        {/* --left side-- */}
+        <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-1/6 bg-red-50 p-4">
+          <Link
+          href={"/"}
+          className="flex items-center gap-2 justify-center lg:justify-start"
+          >
+            <Image 
+             src={"/logo.png"}
+             alt="logo"
+             width={32}
+             height={32}
+            />
+            <span className="hidden lg:block font-semibold">SchoolGlory</span>
+          </Link>
+        </div>
+        {/* --right-side-- */}
+        <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-5/6 bg-blue-50">right</div>
+        </div>    
   );
 }
