ddd416cae0112b530e2a0a6bf0f4a089111e1076
aklesh-dev <arzunayavadu998@gmail.com>
2024-12-16 13:40:47 +0545

    Responsive Navbar Design -Search Bar is design at the start of navbar which is not visible on small device. - Icons and User at the end of the navbar design.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,28 +8,26 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (    
-      <div className="h-screen flex">
-        {/* --left side-- */}
-        <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-1/6 p-4">
-          <Link
+  return (
+    <div className="h-screen flex">
+      {/* --left side-- */}
+      <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-1/6 p-4">
+        <Link
           href={"/"}
           className="flex items-center gap-2 justify-center lg:justify-start"
-          >
-            <Image 
-             src={"/logo.png"}
-             alt="logo"
-             width={32}
-             height={32}
-            />
-            <span className="hidden lg:block font-semibold">SchoolGlory</span>
-          </Link>
-          {/* --Menu component-- */}
-          <Menu />
-        </div>
+        >
+          <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-semibold">SchoolGlory</span>
+        </Link>
+        {/* --Menu component-- */}
+        <Menu />
+      </div>
 
-        {/* --right-side-- */}
-        <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-5/6 bg-blue-50">right</div>
-        </div>    
+      {/* --right-side-- */}
+      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-5/6 bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className='flex items-center justify-between p-4'>
+      {/* --Search Bar-- */}
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+        <Image src={"/search.png"} alt="search" width={14} height={14} />
+        <input type="text" placeholder="search..." className="bg-transparent outline-none p-2 w-[200px]"/>
+      </div>
+
+      {/* --Icon and User */}
+      <div className="flex items-center gap-6 justify-end w-full">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src={'/message.png'} alt="message" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
+          <Image src={'/announcement.png'} alt="message" width={20} height={20} />
+          <div className="absolute -top-3 -right-3 w-5 h-5 bg-purple-500 text-white flex items-center justify-center rounded-full text-xs">1</div>
+        </div>
+        {/* --user-- */}
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">Izumi Dirama</span>
+          <span className="text-[10px] text-right text-gray-500">Admin</span>
+        </div>
+        {/* --avatar */}
+        <Image src={"/avatar.png"} alt="avatar" height={36} width={36} className="rounded-full" />
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
