6425d8f7bbc6ffb66324cd768094992d61b09d09
aklesh-dev <arzunayavadu998@gmail.com>
2024-12-20 15:01:19 +0545

    Student Dashboard Design - install package react-big-calendar, moment and @types/react-big-calendar - create a new component named BigCalendar.tsx - Initialize the calendar in this compnent. - Update the calendar component style to match the design. - Add handleViewChange function to change the calendar view. - update the layout of the dashboard on the right section, so teacher page can have the full height. - Parent and Teacher have the same design with minor changes.

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,23 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 const ParentPage = () => {
   return (
-    <div className=''>ParentPage</div>
+    <div className='p-4 flex flex-col xl:flex-row gap-4 flex-1'>
+      {/* -- Left Side -- */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-svh bg-white rounded-md shadow-md gap-8 p-4">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* -- Right Side -- */}
+      <div className='w-full xl:w-1/3 flex flex-col gap-8'>        
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
-export default ParentPage
+export default ParentPage;
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,25 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex flex-col xl:flex-row gap-4'>
+      {/* -- Left Side -- */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-md shadow-md gap-8 p-4">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* -- Right Side -- */}
+      <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
-export default StudentPage
+export default StudentPage;
