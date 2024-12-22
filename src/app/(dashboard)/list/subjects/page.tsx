f62d9c1364df18f83f346578599b0d2f34344b28
aklesh-dev <arzunayavadu998@gmail.com>
2024-12-22 19:59:41 +0545

    Form modal design - create a form callback function to handle the delete action. - design a form modal for verification before deleting the item.

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,5 +1,4 @@
-// import FormModal from "@/components/FormModal";
-
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -42,17 +41,12 @@ const SubjectListPage = () => {
         {item.teachers.join(", ")}
       </td>
       <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-izumiSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
+        <div className="flex items-center gap-2">          
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-izumiPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            // <FormModal table="teacher" type="delete" id={item.id}/>
+            <>
+            <FormModal table="subject" type="update" data={item}/>
+            <FormModal table="subject" type="delete" id={item.id}/>
+            </>
           )}
         </div>
       </td>
@@ -74,10 +68,7 @@ const SubjectListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-izumiYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-              // <FormModal table="teacher" type="create"/>
+              <FormModal table="subject" type="create"/>
             )}
           </div>
         </div>
--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 const FormModal = ({
   table,
@@ -31,12 +32,47 @@ const FormModal = ({
       : type === "update"
       ? "bg-izumiSky"
       : "bg-izumiPurple";
+
+  const [open, setOpen] = useState(false);
+
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="p-4 flex flex-col gap-4">
+        <h2 className="font-medium text-center">All data will be lost. Are you sure you want to delete this {table}</h2>
+        <div className="flex items-center justify-center gap-4">
+          <button className="bg-red-600 text-white p-2 rounded-md">Yes</button>
+          <button onClick={() => setOpen(false)} className="bg-gray-500 text-white p-2 rounded-md">No</button>
+        </div>
+      </form>
+    ) : (
+      "Create or Update form"
+    )
+  };
+
   return (
     <>
       <button
-        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}>
-          <Image src={`/${type}.png`} alt="" width={16} height={16} />
-        </button>
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
+      </button>
+      {open && (
+        <div className="h-screen w-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div className="absolute top-4 right-4 cursor-pointer hover:-translate-x-1 hover:-translate-y-1 transform transition duration-300">
+              <Image
+                src="/close.png"
+                alt=""
+                width={14}
+                height={14}
+                onClick={() => setOpen(false)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
