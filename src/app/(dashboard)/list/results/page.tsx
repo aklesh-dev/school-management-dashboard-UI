4b779de5aca93082b2d301a6bb0f5e73af0cec0b
aklesh-dev <arzunayavadu998@gmail.com>
2025-01-08 19:33:53 +0545

    Fetch Results - result is being fetch from both exam and assignment table. - condition is made to determine assessment is an exam or an assignment. - update the shortcut link of the student results in the student single page file because studentId is passed directly so we don't need to have classId.

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -1,43 +1,45 @@
-// import FormModal from "@/components/FormModal";
-
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import {  assignmentsData, examsData, resultsData, role } from "@/lib/data";
+import { assignmentsData, examsData, resultsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEMS_PER_PAGE } from "@/lib/settings";
+import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Result = {
+type ResultList = {
   id: number;
-  class: string;
-  subject : string;
-  teacher : string;
-  student : string;
-  type: "exam" | "assignment";
-  date: string;
+  title: string;
+  studentName: string;
+  studentSurname: string;
+  teacherName: string;
+  teacherSurname: string;
   score: number;
+  className: string;
+  startTime: Date;
 };
 
 const columns = [
   {
-    header: "Subject Name",
-    accessor: "name",
+    header: "Title",
+    accessor: "title",
   },
   {
     header: "Student",
     accessor: "student",
-    
-  },  
+  },
   {
     header: "Score",
     accessor: "score",
     className: "hidden md:table-cell",
-  },  
+  },
   {
     header: "Teacher",
     accessor: "teacher",
     className: "hidden md:table-cell",
-  },  
+  },
   {
     header: "Class",
     accessor: "class",
@@ -47,42 +49,142 @@ const columns = [
     header: "Date",
     accessor: "date",
     className: "hidden md:table-cell",
-  },  
+  },
   {
     header: "Actions",
     accessor: "action",
   },
 ];
 
-const ResultListPage = () => {
-  const renderRow = (item: Result) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-izumiPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td className="">{item.student}</td>
-      <td className="hidden md:table-cell">{item.score}</td>
-      <td className="hidden md:table-cell"> {item.teacher} </td>
-      <td className="hidden md:table-cell"> {item.class} </td>
-      <td className="hidden md:table-cell"> {item.date} </td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-izumiSky">                
-              <Image src={"/edit.png"} alt="edit" width={16} height={16}  />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-izumiPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            // <FormModal table="teacher" type="delete" id={item.id}/>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: ResultList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-izumiPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td className="">{item.studentName + " " + item.studentSurname}</td>
+    <td className="hidden md:table-cell">{item.score}</td>
+    <td className="hidden md:table-cell">
+      {" "}
+      {item.teacherName + " " + item.teacherSurname}{" "}
+    </td>
+    <td className="hidden md:table-cell"> {item.className} </td>
+    <td className="hidden md:table-cell">
+      {" "}
+      {new Intl.DateTimeFormat("en-US").format(item.startTime)}{" "}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/teachers/${item.id}`}>
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-izumiSky">
+            <Image src={"/update.png"} alt="edit" width={16} height={16} />
+          </button>
+        </Link>
+        {role === "admin" ||
+          (role === "teacher" && (
+            <>
+              <FormModal table="result" type="update" data={item} />
+              <FormModal table="result" type="delete" id={item.id} />
+            </>
+          ))}
+      </div>
+    </td>
+  </tr>
+);
+
+const ResultListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+
+  // --URL PARAMS CONDITION
+
+  // --Dynamic filtering condition for querying data
+  const query: Prisma.ResultWhereInput = {}; //--Prisma query
+  if (queryParams) {
+    // Check if queryParams exists and is not null/undefined
+    for (const [key, value] of Object.entries(queryParams)) {
+      // Iterate over each key-value pair in the queryParams object
+      if (value !== undefined) {
+        // Ensure the value is defined before processing
+        switch (key) {
+          case "studentId":
+            query.studentId =  value;
+            break;
+          
+          case "search":
+            query.OR = [
+              {exam: {title: {contains: value, mode: "insensitive"}}},
+              {student: {name: {contains: value, mode: "insensitive"}}},
+            ];
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [dataRes, count] = await prisma.$transaction([
+    prisma.result.findMany({
+      where: query,
+      include: {
+        student: { select: { name: true, surname: true } },
+        exam: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true, surname: true } },
+              },
+            },
+          },
+        },
+        assignment: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true, surname: true } },
+              },
+            },
+          },
+        },
+      },
+      take: ITEMS_PER_PAGE,
+      skip: (p - 1) * ITEMS_PER_PAGE,
+    }),
+    prisma.result.count({
+      where: query,
+    }),
+  ]);
+
+  // -- Map over the fetched data array to transform each item
+  const data = dataRes.map((item) => {
+    // -- Determine if the assessment is an exam or an assignment.
+    const assessment = item.exam || item.assignment;
+
+    // --If there's no assessment, return null for this item (skip it)--
+    if (!assessment) return null;
+    // --Check if the assessment has a "startTime" property to idenfify if it's an exam
+    const isExam = "startTime" in assessment;
+
+    // --Return a new object with the desired properties.--
+    return {
+      id: item.id,
+      title: assessment.title,
+      studentName: item.student.name,
+      studentSurname: item.student.surname,
+      teacherName: assessment.lesson.teacher.name,
+      teacherSurname: assessment.lesson.teacher.surname,
+      score: item.score,
+      className: assessment.lesson.class.name,
+      startTime: isExam ? assessment.startTime : assessment.startDate,
+    };
+  });
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -108,9 +210,9 @@ const ResultListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   );
 };
--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -97,7 +97,7 @@ const SingleStudentPage = () => {
             <Link className="p-3 rounded-md bg-izumiSky" href={`/list/lessons?classId=${2}`}>Student&apos;s Lessons</Link>
             <Link className="p-3 rounded-md bg-izumiPurple" href={`/list/teachers?classId=${2}`}>Student&apos;s Teachers</Link>
             <Link className="p-3 rounded-md bg-izumiYellow" href={`/list/exams?classId=${2}`}>Student&apos;s Exams</Link>
-            <Link className="p-3 rounded-md bg-izumiPurpleLight" href={`/list/results?classId=${2}`}>Student&apos;s Results</Link>
+            <Link className="p-3 rounded-md bg-izumiPurpleLight" href={`/list/results?studentId=${"student2"}`}>Student&apos;s Results</Link>
             <Link className="p-3 rounded-md bg-izumiSkyLight" href={`/list/assignments?classId=${2}`}>Student&apos;s Assignments</Link>
           </div>
         </div>
