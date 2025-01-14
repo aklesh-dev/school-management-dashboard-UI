8b2fd3ee3e6072548d41944e6a364c30547e8e56
aklesh-dev <arzunayavadu998@gmail.com>
2025-01-14 21:43:04 +0545

    feat: fetch data according to user role. - fetch user role from the clerk. - use (HOF) higher order function on columns and renderRow based on the role.

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -2,65 +2,80 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { announcementsData, role } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEMS_PER_PAGE } from "@/lib/settings";
+import { auth } from "@clerk/nextjs/server";
 import { Announcement, Class, Prisma } from "@prisma/client";
 import Image from "next/image";
-import Link from "next/link";
+
+const getRole = async () => {
+  const { sessionClaims } = await auth();
+  return (sessionClaims?.metadata as { role?: string })?.role;
+};
 
 type AnnouncementList = Announcement & { class: Class };
 
-const columns = [
-  {
-    header: "Title",
-    accessor: "title",
-  },
-  {
-    header: "Class",
-    accessor: "class",
-    
-  }, 
-  {
-    header: "Date",
-    accessor: "date",
-    className: "hidden md:table-cell",
-  },  
-    
-  {
-    header: "Actions",
-    accessor: "action",
-  },
-];
+const createColumns = (role: string | undefined) => {
+  return [
+    {
+      header: "Title",
+      accessor: "title",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+    },
+    {
+      header: "Date",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    ...(role === 'admin' ? [{
+      header: "Actions",
+      accessor: "action",
+    }] : []),
+  ];
+};
 
-const renderRow = (item: AnnouncementList) => (
-  <tr
-    key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-izumiPurpleLight"
-  >
-    <td className="flex items-center gap-4 p-4">{item.title}</td>
-    <td className="">{item.class.name}</td>
-    <td className="hidden md:table-cell">{new Intl.DateTimeFormat('en-US').format(item.date)}</td>
-    <td>
-      <div className="flex items-center gap-2">       
-        {role === "admin" && (
-          <>
-          {/* --Update Btn-- */}
-          <FormModal table="announcement" type="update" data={item} />
-          {/* --Delete Btn-- */}
-          <FormModal table="announcement" type="delete" id={item.id} />
-        </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
+const createRenderRow = (role: string | undefined) => {
+  const RenderRow = (item: AnnouncementList) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-izumiPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td className="">{item.class.name}</td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat("en-US").format(item.date)}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              {/* --Update Btn-- */}
+              <FormModal table="announcement" type="update" data={item} />
+              {/* --Delete Btn-- */}
+              <FormModal table="announcement" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+  RenderRow.displayName = `RenderRow-${role}`; // Assign a display name
+  return RenderRow;
+};
 
 const AnnouncementListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+
+  const role = await getRole();
+  const renderRow = createRenderRow(role);
+  const columns = createColumns(role);
+
   const { page, ...queryParams } = searchParams;
   const p = page ? parseInt(page) : 1;
 
@@ -74,7 +89,7 @@ const AnnouncementListPage = async ({
       if (value !== undefined) {
         switch (key) {
           case "search":
-             query.title = { contains: value, mode: "insensitive" };
+            query.title = { contains: value, mode: "insensitive" };
             break;
           default:
             break;
@@ -101,7 +116,9 @@ const AnnouncementListPage = async ({
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Announcements</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Announcements
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -112,7 +129,7 @@ const AnnouncementListPage = async ({
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <FormModal table="announcement" type="create"/>
+              <FormModal table="announcement" type="create" />
             )}
           </div>
         </div>
